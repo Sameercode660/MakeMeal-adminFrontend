d436a9bbb523e524a108d8fa2c57908103b7a7c7
Mohd Sameer <mesh789736@gmail.com>
2024-09-08 13:57:55 +0530

    deployment 3.2

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,7 +2,7 @@ import Home from '@/components/Home'
 import ProtectedRoute from '@/components/ProtectedRoute'
 import React from 'react'
 
-export function layout({children}: {children: React.ReactNode}) {
+export default function layout({children}: {children: React.ReactNode}) {
     return (
         <>
             <ProtectedRoute>
@@ -12,4 +12,3 @@ export function layout({children}: {children: React.ReactNode}) {
     )
 }
 
-export default layout
