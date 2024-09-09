3a011d4344928499d1b69f5768a9c753bdc101c2
Mohd Sameer <mesh789736@gmail.com>
2024-09-09 16:20:02 +0530

    deployement 3.5

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -27,26 +27,28 @@ function SignIn() {
 
   async function handleAdminLogin() {
     try {
-      if(!email) {
+      if (!email) {
         setEmailError(true)
         return;
       }
 
-      if(!password) {
+      if (!password) {
         setPasswordError(true)
         return;
       }
 
       setLoading(true)
       const data = {
-        email, 
+        email,
         password
       }
 
       const response = await axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/auth/admin`, data)
       setLoading(false)
       setLogin(true)
-      localStorage?.setItem("name", response.data.response.name)
+      if (typeof window !== undefined) {
+        localStorage?.setItem("name", response.data.response.name)
+      }
       router.push('/home/orders')
 
     } catch (error) {
@@ -57,20 +59,20 @@ function SignIn() {
     <div className="w-full h-screen flex justify-center items-center bg-[url('https://d37byfojjwz7vp.cloudfront.net/new_resize_700_20240719135244200504.png')] bg-no-repeat bg-left">
       <div className='w-[300px] flex flex-col justify-center border rounded h-[400px] p-5 space-y-5'>
         <div className='w-full flex justify-center'>
-          <Image src={Logo} alt="MM" width={150} height={150 } className='rounded-full'></Image>
+          <Image src={Logo} alt="MM" width={150} height={150} className='rounded-full'></Image>
         </div>
         <div>
-          <Input type='text' placeholder='Email' className={`${emailError ? 'border-red-600': ''}`} onChange={(e) => {
+          <Input type='text' placeholder='Email' className={`${emailError ? 'border-red-600' : ''}`} onChange={(e) => {
             setEmail(e.target.value)
           }}></Input>
         </div>
         <div>
-          <Input type='password' placeholder='password' className={`${passwordError ? 'border-red-600': ''}`} onChange={(e) => {
+          <Input type='password' placeholder='password' className={`${passwordError ? 'border-red-600' : ''}`} onChange={(e) => {
             setPassword(e.target.value)
           }}></Input>
         </div>
         <div className='text-center'>
-          <Button variant={'outline'} onClick={handleAdminLogin}>{loading ? 'Please wait..': 'Login'}</Button>
+          <Button variant={'outline'} onClick={handleAdminLogin}>{loading ? 'Please wait..' : 'Login'}</Button>
         </div>
       </div>
     </div >
